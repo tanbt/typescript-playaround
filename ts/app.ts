098b81b120838ace8ca4210efbe6bde132017496
Tan Bui <tan@vaadin.com>
2018-12-19 17:06:09 +0200

    Add interface

--- ts/app.ts
+++ ts/app.ts
@@ -1,8 +1,29 @@
+interface NumberValue {
+    value: number;
+    description: string
+}
+
 export default class Calculator {
+
     static Sum(a: number, b: number): number {
         let c = a + b;
         return c;
     }
+
+    static SumAbs(a: NumberValue, b: NumberValue) : number {
+        let c = a.value + b.value;
+        return c;
+    }
 }
 
-console.log(Calculator.Sum(2,3));
+let a: NumberValue = {
+    value: 3,
+    description: "sth"
+};
+
+let b: NumberValue = {
+    value: 6,
+    description: "sth else"
+};
+
+console.log(Calculator.SumAbs( a, b ));
