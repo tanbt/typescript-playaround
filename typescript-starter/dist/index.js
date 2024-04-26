92e9e43fa85e28125c30d35e91173292e4857db7
tan.bui@virta.global <tan.bui@virta.global>
2024-04-26 20:47:07 +0300

    Update gitignore

--- typescript-starter/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const calculator_1 = require("./util/calculator");
-const calc = new calculator_1.Calculator();
-console.log(calc.add(1, 2));
--- typescript-starter/dist/util/calculator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Calculator = void 0;
-class Calculator {
-    add(a, b) {
-        return a + b;
-    }
-}
-exports.Calculator = Calculator;
