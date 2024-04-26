a51f36dccc8dcceaa55c8cdc3c86f3e501b1a269
tan.bui@virta.global <tan.bui@virta.global>
2024-04-26 20:45:23 +0300

    TS compile and run

---
+++ typescript-starter/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const calculator_1 = require("./util/calculator");
+const calc = new calculator_1.Calculator();
+console.log(calc.add(1, 2));
