63b03cd70364de9eb01ef301f5cd3f8943649125
tan.bui@virta.global <tan.bui@virta.global>
2024-04-28 18:43:22 +0300

    Test subscriber object

--- typescript-design-patterns/src/1-exercise-subscriber/subscriber.spec.ts
+++ typescript-design-patterns/src/1-exercise-subscriber/subscriber.spec.ts
@@ -1,10 +1,23 @@
 import Store from "./Store";
+import { SubscriberInterface } from "./types";
 
 describe("Auth subscriber", () => {
   test("Subscribe and get changes", () => {
     // Init example store Auth state
-    Store.instance.setState("auth", { userId: 12, token: "token" });
-    
-    
+    Store.instance.setState("mockState", { data: "example data" });
+
+    const mockSubscriber = {
+      stateName: "mockState",
+      onUpdate: jest.fn(),
+    };
+    Store.instance.subscribe(mockSubscriber);
+    Store.instance.setState("someState", { data: "updated" });
+    expect(mockSubscriber.onUpdate).not.toHaveBeenCalled();
+
+    Store.instance.setState("mockState", { data: "reald updated" });
+    expect(mockSubscriber.onUpdate).toHaveBeenCalledTimes(1);
+    expect(mockSubscriber.onUpdate.mock.calls[0][0]).toStrictEqual({
+      data: "reald updated",
+    });
   });
 });
