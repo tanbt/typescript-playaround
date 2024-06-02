d416dd66d48545d39e22f2e7c3ffc74e1f0facb2
tan.bui@virta.global <tan.bui@virta.global>
2024-06-02 16:13:30 +0300

    Remove old TS starter learning project

--- typescript-understanding/test/app.test.ts
+++
@@ -1,9 +0,0 @@
-import {expect} from 'chai';
-import Calculator from '../ts/app';
-
-describe('calculator', () => {
-    it('add', () => {
-        let result = Calculator.Sum(2,3);
-        expect(result).equal(5);
-    });
-});
--- typescript-understanding/ts/util/doc.ts
+++
@@ -1,6 +0,0 @@
-export class Util {
-    static output(data: any) {
-        const e = window.document.getElementById('output');
-        e.innerHTML = data;
-    }
-}
